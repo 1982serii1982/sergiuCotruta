7febe6e37d37b1b65926b5466a87767e3fb18a17
1982serii1982 <1982serii1982@gmail.com>
2023-01-25 02:23:42 +0000

    setat designul cu ajutorul grid; extragerea din BD a tuturor si alinierea dupa AZ

--- project2/libs/js/script.js
+++ project2/libs/js/script.js
@@ -50,12 +50,86 @@
             console.log(msg);
     };
 
-    async function getAllEmployee(){
-        let res = await phpRequest("getAll");
-        console.log(res);
+    /***************************************************************************/
+
+    async function getAllEmployee(order, orderBy){
+        order = order.toUpperCase();
+        let res = await phpRequest("getAll", {order: order, orderBy: orderBy});
+        return res;
     }
 
-    getAllEmployee();
+    function tableBuilder(inputData, orderBy){
+        let char = '', mainTemplate, headerTemplate, template, result ='', bodyTemplate = '';
+        inputData.forEach(function(currentVal, index, array){
+
+            if(currentVal[orderBy].charAt(0).toUpperCase() !== char){
+                if(index !== 0){
+                    mainTemplate = `
+                        ${headerTemplate}
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="result_header_wrapper">
+                                        <div class="header_name">Name</div>
+                                        <div class="header_surname">Surname</div>
+                                        <div class="header_email">E-mail</div>
+                                        <div class="header_department">Department</div>
+                                        <div class="header_location">Location</div>
+                                    </div>
+                                    ${bodyTemplate}
+                                </div>
+                            </div>
+                        </div>
+                    `;
+                    result = result.concat(mainTemplate);
+                    bodyTemplate = '';
+                }
+
+                headerTemplate = `<div class="result_header">${currentVal[orderBy].charAt(0).toUpperCase()}</div>`;
+                char = currentVal[orderBy].charAt(0).toUpperCase();
+            }
+
+
+
+            template = `
+                    <div class="result_row_wrapper">
+                        <div class="result_name">${currentVal.firstName}</div>
+                        <div class="result_surname">${currentVal.lastName}</div>
+                        <div class="result_email">${currentVal.email}</div>
+                        <div class="result_department">${currentVal.department}</div>
+                        <div class="result_location">${currentVal.location}</div>
+                    </div>
+            `;
+
+            bodyTemplate = bodyTemplate.concat(template);
+
+            if(index === array.length-1){
+                mainTemplate = `
+                    ${headerTemplate}
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12">
+                                ${bodyTemplate}
+                            </div>
+                        </div>
+                    </div>
+                `;
+                result = result.concat(mainTemplate);
+            }
+        });
+
+        $('.main_wrapper').append(result);
+    }
+
+    async function allEmployeeTableBuilder(order, orderBy){
+        let allEmployeeResult = await getAllEmployee(order, orderBy);
+        tableBuilder(allEmployeeResult, orderBy);
+    }
+
+    /*****************************************************************************/
+
+    
+    allEmployeeTableBuilder('asc', 'location');
 
 })();
 
--- project2/libs/php/getAll.php
+++ project2/libs/php/getAll.php
@@ -2,12 +2,33 @@
 
     require('db.php');
 
+    //$file = 'result.txt';
+
+    $order = $_REQUEST['order'];
+    $orderBy = $_REQUEST['orderBy'];
+
     
-    $sql = "SELECT p.lastName, p.firstName, p.jobTitle, p.id, p.email, d.name AS department, l.name AS location  FROM personnel p LEFT JOIN department d ON (d.id = p.departmentID) LEFT JOIN location l ON (l.id = d.locationID) ORDER BY p.lastName";
-    $result = $conn->query($sql, PDO::FETCH_ASSOC);
+	
+
+    $sql = "SELECT p.lastName, p.firstName, p.jobTitle, p.id, p.email, d.name AS department, l.name AS location  FROM personnel p LEFT JOIN department d ON (d.id = p.departmentID) LEFT JOIN location l ON (l.id = d.locationID) ORDER BY " . $orderBy . " " . $order;
+
+
+    //file_put_contents($file, print_r($sql, true));
+
+
+    $state = $conn->prepare($sql);
+    $state->execute();
+
+    $result = $state->fetchAll(PDO::FETCH_ASSOC);
+
+    $conn = null;
+    $state = null;
+
+    
+    
+    //$result = $conn->query($sql, PDO::FETCH_ASSOC);
     $array =[];
     foreach($result as $row){
-
         array_push($array, $row);
     }
 
